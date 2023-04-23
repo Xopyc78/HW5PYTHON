632f0997facadb8587d6bd398e2d21f335e2e322
Vladislav <andrushkovl@gmail.com>
2023-04-23 23:44:28 +0300

    task26 v2.0

--- task26.py
+++ task26.py
@@ -1,15 +1,19 @@
-# Задача 26: Напишите программу, которая на вход принимает 
-# два числа A и B, и возводит число А в целую степень B с 
+# Задача 26: Напишите программу, которая на вход принимает
+# два числа A и B, и возводит число А в целую степень B с
 # помощью рекурсии.
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 -> 8
-def exponentiation(basis,multiplier):
-    if multiplier ==0:
+
+
+def exponentiation(basis, multiplier):
+    if multiplier == 0:
         return 1
-    if multiplier!=0:
-        return(basis*exponentiation(basis,multiplier-1))
+    if multiplier != 0:
+        return (basis*exponentiation(basis, multiplier-1))
+
+
 print("Введите число, которое надо возвести в степень:")
-basis=int(input())
+basis = int(input())
 print("Введите степень, в которую надо возвести число")
-multiplier=int(input())
-print("Результат возведения в степень:",exponentiation(basis,multiplier))
+multiplier = int(input())
+print("Результат возведения в степень:", exponentiation(basis, multiplier))
